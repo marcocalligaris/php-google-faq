0fb1816acf5fd5674c1b885108f1c68260a81414
marcocalligaris <marcocally@gmail.com>
2022-08-31 16:18:24 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,106 @@
+<?php
+$faq_array = [
+    [
+        'title' => 'Come state implementando',
+        'section' => [
+            'second_title' => '',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate similique id ut molestiae sapiente dolor, laboriosam, nemo saepe dolorem nostrum ab? Eveniet, iusto. Magnam dicta rerum ducimus neque velit temporibus.',
+            'paragraph' => [
+                '',
+                'comma' => [
+                    '',
+                ]
+            ],
+        ],
+    ],
+    [
+        'title' => 'Come fa Google',
+        'section' => [
+            'second_title' => '',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate similique id ut molestiae sapiente dolor, laboriosam, nemo saepe dolorem nostrum ab? Eveniet, iusto. Magnam dicta rerum ducimus neque velit temporibus.',
+            'paragraph' => [
+                '',
+                'comma' => [
+                    '',
+                ]
+            ],
+        ],
+    ],
+    [
+        'title' => 'Perché il mio account',
+        'section' => [
+            'second_title' => '',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate similique id ut molestiae sapiente dolor, laboriosam, nemo saepe dolorem nostrum ab? Eveniet, iusto. Magnam dicta rerum ducimus neque velit temporibus.',
+            'paragraph' => [
+                'La società consociata',
+                'La versione dei termini',
+                'comma' => [
+                    'Google Ireland Limited',
+                    'Google LLC',
+                ],
+            ],
+            'second_title' => 'Stabilire il paese',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate similique id ut molestiae sapiente dolor, laboriosam, nemo saepe dolorem nostrum ab? Eveniet, iusto. Magnam dicta rerum ducimus neque velit temporibus.',
+            'paragraph' => [
+                '',
+                '',
+                'comma' => [
+                    '',
+                    '',
+                ],
+            ],
+        ],
+    ],
+    [
+        'title' => 'Come faccio a rimuovere',
+        'section' => [
+            'second_title' => '',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate similique id ut molestiae sapiente dolor, laboriosam, nemo saepe dolorem nostrum ab? Eveniet, iusto. Magnam dicta rerum ducimus neque velit temporibus.',
+            'paragraph' => [
+                '',
+                'comma' => [
+                    '',
+                ]
+            ],
+        ],
+    ],
+    [
+        'title' => 'Quando faccio clic',
+        'section' => [
+            'second_title' => '',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate similique id ut molestiae sapiente dolor, laboriosam, nemo saepe dolorem nostrum ab? Eveniet, iusto. Magnam dicta rerum ducimus neque velit temporibus.',
+            'paragraph' => [
+                '',
+                'comma' => [
+                    '',
+                ]
+            ],
+        ],
+    ],
+
+];
+
+var_dump($faq_array);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google FAQ</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <!-- Intestazione -->
+    <header></header>
+    <!-- Corpo principale -->
+    <main></main>
+    <!-- Footer -->
+    <footer></footer>
+</body>
+</html>
